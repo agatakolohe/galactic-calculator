49c9ea6f3980cc5685f676364902655f8a13503f
agatakolohe <agatakolohe@gmail.com>
2020-11-13 11:10:33 -0800

    pass jupiter age function

--- __tests__/jupiter.test.js
+++ __tests__/jupiter.test.js
@@ -10,8 +10,8 @@ describe('Jupiter', () => {
   test('should correctly create a Jupiter object', () => {
     expect(reuseableJupiter.age).toEqual(30);
   });
-  test('should correctly calculate the users age on Mars', () => {
-    expect(reuseableJupiter.jupiterAge()).toBeCloseTo(15.957);
+  test('should correctly calculate the users age on Jupiter', () => {
+    expect(reuseableJupiter.jupiterAge()).toBeCloseTo(2.529);
   });
   // test('should correctly calculate the users life expectancy on Mars', () => {
   //   expect(reuseableMars.marsLifeExpectancy()).toBeCloseTo(42.553);
