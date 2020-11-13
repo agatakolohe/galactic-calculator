8a3f3ec06752d3bad6918fccb961cab53470750d
agatakolohe <agatakolohe@gmail.com>
2020-11-13 12:48:05 -0800

    test youmaybedead function pass

--- __tests__/jupiter.test.js
+++ __tests__/jupiter.test.js
@@ -20,6 +20,13 @@ describe('Jupiter', () => {
     expect(reuseableJupiter.timeLeftOnJupiter()).toBeGreaterThanOrEqual(4.21);
   });
   test('should correctly let the user know if they are still alive', () => {
-    expect(reuseableJupiter.youMayBeDead()).toBe(`Congratulations, somehow you are still alive on Jupiter.`)
+    expect(reuseableJupiter.youMayBeDead()).toBe(`Congratulations, somehow you are still alive on Jupiter.`);
+  });
+});
+
+describe('Jupiter if user lives past life expectancy', () => {
+  test('should correctly let the user know if they are dead', () => {
+    let maybeDead = new Jupiter(90);
+    expect(maybeDead.youMayBeDead()).toBe(`Congratulations, technology has sufficiently advanced and you have surpassed the time projected for you to survive on Jupiter. Or it didn't, and you dead.`);
   });
 });
