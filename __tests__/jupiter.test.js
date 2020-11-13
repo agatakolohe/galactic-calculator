dba19f14013beedab18cc7fa0494594b756ecc50
agatakolohe <agatakolohe@gmail.com>
2020-11-13 11:09:30 -0800

    test jupiter age function good fail

--- __tests__/jupiter.test.js
+++ __tests__/jupiter.test.js
@@ -10,9 +10,9 @@ describe('Jupiter', () => {
   test('should correctly create a Jupiter object', () => {
     expect(reuseableJupiter.age).toEqual(30);
   });
-  // test('should correctly calculate the users age on Mars', () => {
-  //   expect(reuseableMars.marsAge()).toBeCloseTo(15.957);
-  // });
+  test('should correctly calculate the users age on Mars', () => {
+    expect(reuseableJupiter.jupiterAge()).toBeCloseTo(15.957);
+  });
   // test('should correctly calculate the users life expectancy on Mars', () => {
   //   expect(reuseableMars.marsLifeExpectancy()).toBeCloseTo(42.553);
   // });
