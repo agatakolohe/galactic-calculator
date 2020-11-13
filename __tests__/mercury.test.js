07b5f5bad1e3cf2b1cff06e1a298e303ce8bde3f
agatakolohe <agatakolohe@gmail.com>
2020-11-13 09:36:33 -0800

    test mercury constructor good fail

--- __tests__/mercury.test.js
+++ __tests__/mercury.test.js
@@ -6,4 +6,8 @@ describe('Mercury', () => {
   beforeEach(() => {
     reuseableMercury = new Mercury(30);
   });
+
+  test('should correctly create a Mercury object', () => {
+    expect(reuseableMercury.age).toEqual(30);
+  });
 });
