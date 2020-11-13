182edfb5e7902f7e8894a04b379a0ef807734579
agatakolohe <agatakolohe@gmail.com>
2020-11-13 10:36:59 -0800

    test venus age function good fail

--- __tests__/venus.test.js
+++ __tests__/venus.test.js
@@ -10,4 +10,7 @@ describe('Venus', () => {
   test('should correctly create a Venus object', () => {
     expect(reuseableVenus.age).toEqual(30);
   });
+  test('should correctly calculate the users age on Venus', () => {
+    expect(reuseableVenus.venusAge()).toBeCloseTo(48);
+  });
 });
