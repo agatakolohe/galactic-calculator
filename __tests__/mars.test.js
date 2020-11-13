5115765063c776e117fd99a2895b92655dd025d1
agatakolohe <agatakolohe@gmail.com>
2020-11-13 10:53:28 -0800

    test mars age function good fail

--- __tests__/mars.test.js
+++ __tests__/mars.test.js
@@ -10,9 +10,9 @@ describe('Mars', () => {
   test('should correctly create a Mars object', () => {
     expect(reuseableMars.age).toEqual(30);
   });
-  // test('should correctly calculate the users age on Mars', () => {
-  //   expect(reuseableMars.marsAge()).toBeCloseTo(48.387);
-  // });
+  test('should correctly calculate the users age on Mars', () => {
+    expect(reuseableMars.marsAge()).toBeCloseTo(48.387);
+  });
   // test('should correctly calculate the users life expectancy on Mars', () => {
   //   expect(reuseableMars.marsLifeExpectancy()).toBeCloseTo(129.032);
   // });
