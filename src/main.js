1205ff9a67baafa205a0d032cb8225212f13fadd
agatakolohe <agatakolohe@gmail.com>
2020-11-15 13:28:05 -0800

    test to see if jupiter age will display on submit, pass

--- src/main.js
+++ src/main.js
@@ -7,11 +7,19 @@ import Venus from './js/venus.js';
 import Jupiter from './js/jupiter.js';
 import Mercury from './js/mercury.js';
 
+
+
 $(document).ready(function() {
   $("#calculator").submit(function(event) {
     event.preventDefault();
     const inputtedAge = parseInt($("input#age").val());
     const mars = new Mars(inputtedAge);
+    const venus = new Venus(inputtedAge);
+    const jupiter = new Jupiter(inputtedAge);
+    const mercury = new Mercury(inputtedAge);
+
+   const showJupiter = jupiter.jupiterAge();
+   $("#jupiter-response").text(showJupiter);
 
 console.log(mars);
 console.log(Venus);
