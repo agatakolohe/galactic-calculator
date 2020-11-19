a6fafe8010ec9be596e70e766ed8d896c00f6896
agatakolohe <agatakolohe@gmail.com>
2020-11-18 19:31:35 -0800

    test time left on venus if age 200, good fail

--- __tests__/venus.test.js
+++ __tests__/venus.test.js
@@ -2,9 +2,11 @@ import Venus from './../src/js/venus.js'
 
 describe('Venus', () => {
   let reuseableVenus;
+  let youDead;
 
   beforeEach(() => {
     reuseableVenus = new Venus(30);
+    youDead = new Venus(200);
   });
 
   test('should correctly create a Venus object', () => {
@@ -16,7 +18,7 @@ describe('Venus', () => {
   test('should correctly calculate the users life expectancy on Venus', () => {
     expect(reuseableVenus.venusLifeExpectancy()).toBeGreaterThanOrEqual(129.03);
   });
-  test('should correctly calculate the users time left on Venus', () => {
-    expect(reuseableVenus.timeLeftOnVenus()).toBeGreaterThanOrEqual(80.64);
+  test('should correctly check if the user is dead on Venus', () => {
+    expect(youDead.timeLeftOnVenus()).toEqual(`Congratulations, technology has sufficiently advanced and you have surpassed the time projected, -193 years, for you to survive on Venus. Or it didn't, and you have been dead for 193 years.`);
   });
 });
--- src/js/venus.js
+++ src/js/venus.js
@@ -9,6 +9,11 @@ export default class Venus {
     return 80/.62;
   }
   timeLeftOnVenus () {
-    return (this.venusLifeExpectancy() - this.venusAge());
+    let result = (this.venusLifeExpectancy() - this.venusAge());
+    if (result < 0) {
+      return `Congratulations, technology has sufficiently advanced and you have surpassed the time projected, ${result} years, for you to survive on Venus. Or it didn't, and you have been dead for ${result} years.`
+    } else {
+      return `You have ${result} years left to live on Venus. Treat yo self!`
+    }
   }
 }
