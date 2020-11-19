817c40d1239ad4080dcbd04849936de501cf6a78
agatakolohe <agatakolohe@gmail.com>
2020-11-18 19:41:54 -0800

    change timeLeftOnMercury function, test if user dead, good fail

--- __tests__/mercury.test.js
+++ __tests__/mercury.test.js
@@ -2,9 +2,11 @@ import Mercury from './../src/js/mercury.js'
 
 describe('Mercury', () => {
   let reuseableMercury;
+  let youDead;
 
   beforeEach(() => {
     reuseableMercury = new Mercury(30);
+    youDead = new Mercury(200);
   });
 
   test('should correctly create a Mercury object', () => {
@@ -16,7 +18,7 @@ describe('Mercury', () => {
   test('should correctly calculate the users life expectancy on Mercury', () => {
     expect(reuseableMercury.mercuryLifeExpectancy()).toBeGreaterThanOrEqual(333.33);
   });
-  test('should correctly calculate the users time left on Mercury', () => {
-    expect(reuseableMercury.timeLeftOnMercury()).toBeGreaterThanOrEqual(208.33);
+  test('should correctly check if the user is dead on Mercury', () => {
+    expect(youDead.timeLeftOnMercury()).toEqual(`Congratulations, technology has sufficiently advanced and you have surpassed the time projected, -193.54838709677418 years, for you to survive on Mercury. Or it didn't, and you have been dead for -193.54838709677418 years.`);
   });
 });
--- src/js/mercury.js
+++ src/js/mercury.js
@@ -9,6 +9,11 @@ export default class Mercury {
     return 80/.24;
   }
   timeLeftOnMercury () {
-    return (this.mercuryLifeExpectancy() - this.mercuryAge());
+    let result = (this.mercuryLifeExpectancy() - this.mercuryAge());
+    if (result < 0) {
+      return `Congratulations, technology has sufficiently advanced and you have surpassed the time projected, ${result} years, for you to survive on Mercury. Or it didn't, and you have been dead for ${result} years.`
+    } else {
+      return `You have ${result} years left to live on Mercury. Treat yo self!`
+    }
   }
 }
